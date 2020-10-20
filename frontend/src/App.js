2ea3eb2306134542092a44d14f4e79b991c2dc2b
Kevin Ibanez <Kpeibanez@gmail.com>
2020-10-20 14:43:06 -0500

    moved project onto github

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {Container} from 'react-bootstrap'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import HomeScreen from './screens/HomeScreen.jsx'
+import ProductScreen from './screens/ProductScreen'
+
+const  App = () => {
+  return (
+    <Router>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <Route path='/' component={HomeScreen}exact />
+        <Route path='/product/:id' component={ProductScreen} />
+      </Container>
+    </main>
+    <Footer />
+    </Router>
+  );
+}
+
+export default App;
