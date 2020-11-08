717695593b946a98c845a2324e7bf5ab494eca03
Kevin Ibanez <Kpeibanez@gmail.com>
2020-11-07 20:23:41 -0600

    added import data and destroy data functionality with test data

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -19,16 +19,60 @@ const orderSchema = mongoose.Schema({
         }
     ],
     shippingAddress: {
-        type: String,
-        required: true
+        address: {type: String, required: true},
+        city: {type: String, required: true},
+        postalCode: {type: String, required: true},
+        country: {type: String, required: true},
     },
-    isAdmin: {
-        type: boolean,
+    paymentMethod: {
+        type: String,
         required: true,
-        default:false
+    },
+    paymentMethod: {
+        type: String,
+        required: true,
+    },
+    paymentResult: {
+        id: { type: String},
+        status: { type: String},
+        update_time: { type: String},
+        email_address: { type: String},
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default:0.0
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default:0.0
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default:0.0
+    },
+    isPaid: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    paidAt: {
+        type: Date
+    },
+    isDelivered: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    deliveredAt: {
+        type: Date,
     }
 }, {
     timestamps: true
 })
 
-const User = mongoose.model('User', userSchema)
+const Order = mongoose.model('Order', orderSchema)
+
+export default Order
