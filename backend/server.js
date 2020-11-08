480e5c36855a76d2b0463ee819ce7a49f6de1a8c
Kevin Ibanez <kpeibanez@gmail.com>
2020-11-08 17:40:34 -0600

    add to cart functionality including quantity functionality

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,22 @@ import dotenv from 'dotenv'
 import express from 'express'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
+
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.get('/', (req, res) => {
     res.send('API is running....')
